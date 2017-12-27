a455a8d71bcb6a51d34b2de18a6093dc12fffb46
Jakub Wilkowski <jakub.wilkowski@10clouds.com>
2017-12-27 12:00:33 +0100

    Chapter 4: What Are We Doing with All These Tests? (And, Refactoring)

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
 # Create your tests here.
 
 class HomePageTest(TestCase):
-    
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -17,5 +18,10 @@ class HomePageTest(TestCase):
         response = home_page(request)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>Listy rzeczy do zrobienia</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
--- superlists/tests/functional_tests.py
+++ superlists/tests/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,35 +13,49 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do-app. She goes
         # to check out it's homepage
-        
+
         self.browser.get('http://localhost:8000')
-        
+
         # She notices the page title and header mention to-do lists.
-        
+
         self.assertIn('Listy rzeczy do zrobienia', self.browser.title)
-        self.fail('Zakończenie testu')
-        
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('lista', header_text)
+
         # She is invited to enter a to-do item straight away
-        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Wpisz rzecz do zrobienia'
+        )
+
         # She types "buy peacock feathers" into a text box (Edith's hobby
         # is typing fly-fishing lures
-        
+        inputbox.send_keys('Kupić pawie pióra')
+
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Kupić pawie pióra' for row in rows),
+            "Nowy element nie znajduje się w tabeli"
+        )
+
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        
+        self.fail('Zakończenie testu')
+
         # The page updates again, and now shows both items on her list
-        
+
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-        
+
         # She visits that URL - her to-do list is still there.
-        
+
         # Satisfied, she goes back to sleep
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
